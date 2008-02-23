6ad09bb780d07520737d16b5f7e37dbf56cb901b
unknown <venkatre@mskcc.org>
2008-02-23 18:04:48 +0000

    Added segments.summary command to give sd, median and mad of segments in addition to the mean.  Changed Venkat's affiliation in the vignette.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DNAcopy@30119 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/segmentsummary.R
@@ -0,0 +1,31 @@
+segments.summary <- function(x)
+  {
+    if (!inherits(x, "DNAcopy")) 
+      stop("First arg must be the result of segment")
+    xdat <- x$data
+    xout <- x$output
+    nsample <- ncol(xdat)-2
+    sampleid <- colnames(xdat)[-(1:2)]
+    seg.median <- seg.sd <- seg.mad <- rep(NA, nrow(xout))
+    ll <- 0
+    iisamp <- 2
+    for (isamp in sampleid) {
+      iisamp <- iisamp + 1
+# genomdat = logratio data of sample isamp
+      genomdat <- xdat[, iisamp]
+# ina = location of the missing values and infinity
+      ina <- which(!is.na(genomdat) & !(abs(genomdat) == Inf))
+# subset out the missing & infinity locations
+      genomdat <- genomdat[ina]
+      seglen <- xout$num.mark[xout$ID == isamp]
+      kk <- length(seglen)
+      seg.sd[ll+(1:kk)] <- tapply(genomdat, rep(1:kk,seglen), sd)
+      seg.median[ll+(1:kk)] <- tapply(genomdat, rep(1:kk,seglen), median)
+      seg.mad[ll+(1:kk)] <- tapply(genomdat, rep(1:kk,seglen), mad)
+      ll <- ll + kk
+    }
+    xout$seg.sd <- round(seg.sd, 4)
+    xout$seg.median <- round(seg.median, 4)
+    xout$seg.mad <- round(seg.mad, 4)
+    xout
+  }
