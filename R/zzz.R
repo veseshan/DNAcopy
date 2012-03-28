c1e3a9bfd674719d3ffe7963a259095c598bd932
unknown <venkatre@mskcc.org>
2012-03-28 17:33:43 +0000

    Dropped startup message about change in data format.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DNAcopy@64559 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
 .onLoad <- function(libname, pkgname) {
   library.dynam("DNAcopy", pkgname, libname)
-  packageStartupMessage("\n**************************************************************************\n   The plan to change the data format for CNA object has been postponed   \n in order to ensure backward compatibility with older versions of DNAcopy \n**************************************************************************\n")
 }
