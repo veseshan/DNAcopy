6847d93e0a3c6b3b7a1031226509c75430417eed
unknown <venkatre@mskcc.org>
2016-11-23 19:57:58 +0000

    Changed length to length.out in seq call (strict name match - Thanks HB)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DNAcopy@124418 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/changepoints.R
+++ R/changepoints.R
@@ -188,7 +188,7 @@ trimmed.variance <- function(genomdat, trim=0.025)
 inflfact <- function(trim)
   {
     a <- qnorm(1-trim)
-    x <- seq(-a,a,length=10001)
+    x <- seq(-a,a,length.out=10001)
     x1 <- (x[-10001] + x[-1])/2
     1/(sum(x1^2*dnorm(x1)/(1-2*trim))*(2*a/10000))
   }
